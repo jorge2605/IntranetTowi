d107cf637763a3ab1b14fda5dc9b93ee19b028d5
Jorge Santacruz <santacruz.leonel.1h@hotmail.com>
2026-01-06 13:06:04 -0700

    trabajo para 2

--- IntranetTowi/src/dashboard/Dashboard.jsx
+++ IntranetTowi/src/dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ const Dashboard = () => {
         <div className="contenido">
           <div className='oeste'>
             <Icono img="https://cdn.iconscout.com/icon/premium/png-512-thumb/orden-icon-svg-download-png-7083890.png?f=webp&w=512" titulo="Requisiciones"/>
+            <Icono img="https://cdn.iconscout.com/icon/premium/png-512-thumb/gerente-de-proyecto-icon-svg-download-png-5563439.png?f=webp&w=512" titulo="Proyectos"/>
             <Icono img="https://cdn.iconscout.com/icon/premium/png-512-thumb/usuario-icon-svg-download-png-8063145.png?f=webp&w=512" titulo="Usuario"/>
             <Icono img="https://cdn.iconscout.com/icon/premium/png-512-thumb/salir-de-pantalla-completa-icon-svg-download-png-4827491.png?f=webp&w=512" titulo="Salir" salir={true}/>
           </div>
